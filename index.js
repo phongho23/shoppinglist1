ac17c7e4802fa7bdcf43620a8b4f7ff1b06b9274
phongho23 <36687809+phongho23@users.noreply.github.com>
2020-10-09 11:52:08 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+function listInteract() {
+    $('#js-shopping-list-form').submit( function(event) {
+        //stop default form behavior
+        event.preventDefault();
+
+        const userTextListItem = $("#shopping-list-entry").val();
+
+        if (userTextListItem !== "") {
+        $('.shopping-list').append(`<li>
+        <span class="shopping-item">${userTextListItem}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`
+      );
+
+        $("#shopping-list-entry").val("");
+        }
+    });
+
+    $('.shopping-list').on('click', '.shopping-item-toggle', function (event) {
+        $(this).closest('.shopping-item-controls').siblings('.shopping-item').toggleClass('shopping-item__checked');
+    });
+
+    $('.shopping-list').on('click', '.shopping-item-delete', function (event) {
+        $(this).closest('li').remove();
+    });
+}
+    
+$(listInteract);
+
